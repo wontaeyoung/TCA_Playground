9e55008bd8a77933d3c5f62e1c6364a64b34409b
Taeyoung Won <hexter1994@gmail.com>
2023-10-01 17:55:51 +0900

    [Add] CounterFeature 리듀서 추가, State 및 Action 정의

--- TCA-Playground/ContentView.swift
+++ TCA-Playground/ContentView.swift
@@ -1,6 +1,49 @@
 import ComposableArchitecture
 import SwiftUI
 
+struct CounterFeature: Reducer {
+    struct State {
+        var number: Int = 0
+        var factString: String? = nil
+        var isTimerOn: Bool = false
+    }
+    
+    enum Action {
+        case incrementButtonTapped
+        case decrementButtonTapped
+        case getFactButtonTapped
+        case toggleTimerButtonTapped
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .incrementButtonTapped:
+                state.number += 1
+                return .none
+                
+            case .decrementButtonTapped:
+                decreaseNumber(num: &state.number)
+                return .none
+                
+            case .getFactButtonTapped:
+                return .none
+                
+            case .toggleTimerButtonTapped:
+                state.isTimerOn.toggle()
+                return .none
+                
+            }
+        }
+    }
+}
+
+private extension CounterFeature {
+    func decreaseNumber(num: inout Int) {
+        num -= 1
+    }
+}
+
 struct ContentView: View {
     let store: Store<CounterFeature.State, CounterFeature.Action>
     
