add7a324fa731e2b1a9c9a113edaa0a82f192042
Taeyoung Won <hexter1994@gmail.com>
2023-10-01 16:19:16 +0900

    [Build Up] Project Init

---
+++ TCA-Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-Playground
+//
+//  Created by 원태영 on 10/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA-Playground/TCA_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PlaygroundApp.swift
+//  TCA-Playground
+//
+//  Created by 원태영 on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
