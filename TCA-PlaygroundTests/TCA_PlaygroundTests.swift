4eb7035b519c125cb98a2d8c6ea97f212ec726c2
Taeyoung Won <hexter1994@gmail.com>
2023-10-02 23:37:49 +0900

    [Test] 타이머 토글 테스트 케이스 추가

--- TCA-PlaygroundTests/TCA_PlaygroundTests.swift
+++ TCA-PlaygroundTests/TCA_PlaygroundTests.swift
@@ -11,15 +11,15 @@ import XCTest
 
 @MainActor
 final class TCA_PlaygroundTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func test증가버튼을눌러서1이되었다가감소버튼을눌러서다시0이된다() async {
         let store = TestStore(initialState: CounterFeature.State()) {
             CounterFeature()
@@ -39,6 +39,18 @@ final class TCA_PlaygroundTests: XCTestCase {
         }
     }
     
+    func test타이머가토글되면값이변경된다() async {
+        let store = TestStore(
+            initialState: CounterFeature.State()
+        ) {
+            CounterFeature()
+        }
+        
+        await store.send(.toggleTimerButtonTapped) { state in
+            state.isTimerOn = true
+        }
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -46,12 +58,12 @@ final class TCA_PlaygroundTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
