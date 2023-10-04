b5cc24bd5a54a403cfa027b4572794d1efe91e20
Taeyoung Won <hexter1994@gmail.com>
2023-10-04 23:59:59 +0900

    [Add] numberFact 의존성 키 추가

---
+++ TCA-Playground/DependencyValues+.swift
@@ -0,0 +1,8 @@
+import ComposableArchitecture
+
+extension DependencyValues {
+    var numberFact: NumberFactClient {
+        get { self[NumberFactClient.self] }
+        set { self[NumberFactClient.self] = newValue }
+    }
+}
